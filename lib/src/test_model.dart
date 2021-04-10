424c7f2cdde2d6e8f98dd56f099398951807e18b
Arkangel12 <argel.bc18@gmail.com>
2021-04-10 06:11:22 -0700

    add packages for json serializable and test_model class

---
+++ lib/src/test_model.dart
@@ -0,0 +1,15 @@
+class TestModel {
+  final int id;
+  final double idDouble;
+  final String name;
+  final bool isValid;
+  final List<int> listIds;
+
+  TestModel({
+    required this.id,
+    required this.idDouble,
+    required this.name,
+    required this.isValid,
+    required this.listIds,
+  });
+}
